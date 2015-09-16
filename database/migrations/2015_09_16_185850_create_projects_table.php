e1c3d0cf17b65d8707b8724b1efc98220b64c38c
Justin Christenson <info@Justinc.me>
2015-09-16 16:09:50 -0700

    Implemented projects table migration

--- database/migrations/2015_09_16_185850_create_projects_table.php
+++ database/migrations/2015_09_16_185850_create_projects_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
 class CreateProjectsTable extends Migration
 {
@@ -12,7 +13,20 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('projects', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('category');
+            $table->foreign('category')->references('id')->on('categories');
+            //Project Images are loaded via Eloquent relationship
+            //Skills are loaded via Eloquent relationship
+            $table->string('name');
+            $table->string('live_url')->nullable();
+            $table->string('github_url')->nullable();
+            $table->text('case_study');
+            $table->text('testimonial')->nullable();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +36,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('projects');
     }
 }

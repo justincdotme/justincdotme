4876d5583314c00cba5b673b5f47a3e3baebc36c
Justin Christenson <info@Justinc.me>
2015-09-16 16:04:21 -0700

    Implemented pages table migration

--- database/migrations/2015_09_16_185833_create_pages_table.php
+++ database/migrations/2015_09_16_185833_create_pages_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePagesTable extends Migration
 {
@@ -12,7 +13,15 @@ class CreatePagesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('pages', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('title');
+            $table->string('description');
+            $table->string('keywords');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +31,6 @@ class CreatePagesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('pages');
     }
 }

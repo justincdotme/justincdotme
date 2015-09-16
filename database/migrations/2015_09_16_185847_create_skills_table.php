dd0efe7dfb3276957c6c4941537fb5c44a6b6893
Justin Christenson <info@Justinc.me>
2015-09-16 16:08:01 -0700

    Created skills table migration

---
+++ database/migrations/2015_09_16_185847_create_skills_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

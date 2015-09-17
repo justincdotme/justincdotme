b78b649443da4eae81c2003c68e589dae73a06ef
Justin Christenson <info@Justinc.me>
2015-09-17 15:58:34 -0700

    Added project_skill pivot table Added ProjectSkillTable seeder

---
+++ database/seeds/ProjectSkillsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectSkillsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

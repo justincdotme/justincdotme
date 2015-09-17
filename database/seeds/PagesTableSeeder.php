57947c6a0b43ecb8d7efacbf0aebe4de76830f29
Justin Christenson <info@Justinc.me>
2015-09-17 16:30:37 -0700

    Implemented PagesTableSeeder

--- database/seeds/PagesTableSeeder.php
+++ database/seeds/PagesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PagesTableSeeder extends Seeder
 {
@@ -11,6 +12,32 @@ class PagesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pages')->insert([
+            'name'          => 'home',
+            'title'         => 'Justin Christenson is a web Developer in Vancouver, WA',
+            'description'   => 'Justin Christenson is a Web Developer specializing in PHP applications.',
+            'keywords'      => 'web developer, php, laravel, developer, mysql, database, javascript, html, css, portland, vancouver'
+        ]);
+
+        DB::table('pages')->insert([
+            'name'          => 'bio',
+            'title'         => 'Justin Christenson - Web Developer - Bio',
+            'description'   => 'The biography of Justin Christenson, Web Developer',
+            'keywords'      => 'web developer, php, laravel, developer, mysql, database, javascript, html, css, portland, vancouver'
+        ]);
+
+        DB::table('pages')->insert([
+            'name'          => 'portfolio',
+            'title'         => 'Justin Christenson - Review my Portfolio',
+            'description'   => 'The professional portfolio of Justin Christenson, Web Developer',
+            'keywords'      => 'justin christenson, portfolio, web developer, php, laravel, developer, mysql, database, javascript, html, css'
+        ]);
+
+        DB::table('pages')->insert([
+            'name'          => 'contact',
+            'title'         => 'Contact Justin Christenson',
+            'description'   => 'Contact Justin Christenson',
+            'keywords'      => 'justin christenson, contact, phone, email, linkedin, twitter, github, git hub, google plus, google+'
+        ]);
     }
 }

66284831e008bdcebe74267397675708832e698c
Justin Christenson <info@Justinc.me>
2015-09-17 11:52:13 -0700

    Added PagesTableSeeder Added ProjectCategoriesTableSeeder Added ProjectImagesTableSeeder Added ProjectsTableSeeder Added SkillsTableSeeder Added UsersTableSeeder

---
+++ database/seeds/ProjectCategoriesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProjectCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

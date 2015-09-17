36b1607efb6db1b852aea912d30dfded611065c7
Justin Christenson <info@Justinc.me>
2015-09-17 16:00:16 -0700

    Implemented project_skill migration

--- database/migrations/2015_09_16_185852_create_project_skill_table.php
+++ database/migrations/2015_09_16_185852_create_project_skill_table.php
@@ -13,7 +13,17 @@ class CreateProjectSkillTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('project_skill', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned();
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects');
+            $table->integer('skill_id')->unsigned();
+            $table->foreign('skill_id')
+                ->references('id')
+                ->on('skills');
+        });
     }
 
     /**
@@ -23,6 +33,6 @@ class CreateProjectSkillTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('project_skill');
     }
 }

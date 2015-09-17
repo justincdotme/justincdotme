95bcf024931f2ee6a2d0122d7c1cfa1d8b260dd4
Justin Christenson <info@Justinc.me>
2015-09-17 16:32:30 -0700

    Implemented SkillsTableSeeder

--- database/seeds/SkillsTableSeeder.php
+++ database/seeds/SkillsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SkillsTableSeeder extends Seeder
 {
@@ -11,6 +12,41 @@ class SkillsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('skills')->insert([
+            'skill' => 'Laravel 4'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'Laravel 5'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'Object Oriented PHP'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'MySQL'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'Javascript & jQuery'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'AngularJS'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'HTML & CSS (SCSS)'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'Web Server Administration'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'Wordpress Theme Development'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'ORM (Eloquent)'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'REST API Development'
+        ]);
+        DB::table('skills')->insert([
+            'skill' => 'REST API Consumption'
+        ]);
     }
 }

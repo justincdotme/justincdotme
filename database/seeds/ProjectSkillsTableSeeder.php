868b586066ae5b01ad10fe78c095560c16cab7d6
Justin Christenson <info@Justinc.me>
2015-09-17 16:33:08 -0700

    Implemented ProjectSkillsTableSeeder

--- database/seeds/ProjectSkillsTableSeeder.php
+++ database/seeds/ProjectSkillsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProjectSkillsTableSeeder extends Seeder
 {
@@ -11,6 +12,212 @@ class ProjectSkillsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('project_skill')->insert([
+            'project_id'    => 1,
+            'skill_id'      => 1
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 1,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 1,
+            'skill_id'      => 4
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 1,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 1,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 1,
+            'skill_id'      => 8
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 2,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 2,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 2,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 3,
+            'skill_id'      => 9
+        ]);
+
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 3,
+            'skill_id'      => 3
+        ]);
+
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 3,
+            'skill_id'      => 5
+        ]);
+
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 3,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 2
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 4
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 6
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 8
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 10
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 4,
+            'skill_id'      => 11
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 5,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 5,
+            'skill_id'      => 4
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 5,
+            'skill_id'      => 10
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 5,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 5,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 6,
+            'skill_id'      => 2
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 6,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 6,
+            'skill_id'      => 4
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 6,
+            'skill_id'      => 10
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 6,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 6,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 7,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 7,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 7,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 7,
+            'skill_id'      => 12
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 8,
+            'skill_id'      => 3
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 8,
+            'skill_id'      => 5
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 8,
+            'skill_id'      => 7
+        ]);
+
+        DB::table('project_skill')->insert([
+            'project_id'    => 8,
+            'skill_id'      => 12
+        ]);
     }
 }

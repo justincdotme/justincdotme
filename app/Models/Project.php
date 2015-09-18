11f3210b06dbccf4cb87c09d18dd4941eac2a3e2
Justin Christenson <info@Justinc.me>
2015-09-18 09:43:22 -0700

    Implemented Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    /**
+     * Load project images 1-n relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function images()
+    {
+        return $this->hasMany('App\Models\ProjectImage');
+    }
+
+    /**
+     * Load project skills n-n relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function skills()
+    {
+        return $this->belongsToMany('App\Models\Skill');
+    }
 }

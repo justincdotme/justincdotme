a54a8093c1a8053dec82afc26ff6bbbe1b71156e
Justin Christenson <info@Justinc.me>
2015-09-28 16:46:45 -0700

    Extracted GA code to analytics.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,18 +34,14 @@
 <!--[if lt IE 8]>
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
-<h1>home page</h1>
+<!-- Begin Body-->
 
+
+
+<!-- End Body-->
 <script src="{{ elixir('js/scripts.js') }}"></script>
 
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='//www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
+@include('partials.analytics')
+
 </body>
 </html>

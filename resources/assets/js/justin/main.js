c1c6032c7b026889b76c850eb169ab757de9878a
Justin Christenson <info@Justinc.me>
2015-09-29 13:17:12 -0700

    Added new icons Updated gitignore Removed HTML5 boilerplate favicons (yuck) Ported old content to new page layout

---
+++ resources/assets/js/justin/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $window = $(window);
+
+    //Set pages min-height to 100% window height
+    winHeight = $window.height();
+    $('.pages').each(function()
+    {
+        elementHeight = $(this).height();
+        if(elementHeight < winHeight) {
+            $(this).height(winHeight);
+        }
+    });
+
+    //Parallax scrolling
+    $('section[data-type="background"]').each(function(){
+        var $jThis = $(this);
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $jThis.data('speed'));
+            var coords = '50% '+ yPos + 'px';
+            $jThis.css({
+                backgroundPosition: coords
+            });
+        });
+    });
+});

d2d2a3b3067afff1900cbc6242cf286f1dc8768e
Justin Christenson <info@Justinc.me>
2015-09-28 18:21:47 -0700

    Updated favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,27 @@
     <meta name="format-detection" content="telephone=no"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="vW4bzfGyDJ8ZCah1pLjl2ka-B0NpyFHtgt0WPiF3heA"/>
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/img/ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/img/ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/img/ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/img/ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/img/ico/img/ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/img/ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/img/ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/img/ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/img/ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/img/ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/ico/favicon-16x16.png">
+
+    <link rel="manifest" href="/img/ico/manifest.json">
+
+    <meta name="msapplication-TileColor" content="#222222">
+    <meta name="msapplication-TileImage" content="/img/ico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#222222">
+
     <link rel="author" href="https://plus.google.com/115474380698631066615/posts"/>
     <link href='http://fonts.googleapis.com/css?family=Raleway+Dots|Open+Sans' rel='stylesheet' type='text/css'/>
     <link rel="stylesheet" href="/css/bootstrap.min.css">

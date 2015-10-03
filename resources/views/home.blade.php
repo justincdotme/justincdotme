b7873c966886020e7d81a38b5f9a4ea949e41e24
Justin Christenson <info@Justinc.me>
2015-10-02 17:00:49 -0700

    Updated portfolio description heading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -217,7 +217,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 project-details">
-                                <h3 class="project-detail">Details</h3>
+                                <h3 class="project-case-study">Case Study</h3>
                                 <p class="indent">Fawkes FX is a small creative firm located in Portland, OR that I helped establish. We required a public website to showcase our services. The website also reuired a private administration interface with transactional email integration.</p>
                                 <p class="indent last-paragraph">The front end team at FawkesFX designed and built the public website, which I merged into a Laravel 4 application. I then designed and developed the administration interface featuring several custom modules including a user management module, an SEO module and a leads management module.</p>
                                 <p class="text-center">
@@ -264,7 +264,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 project-details">
-                                <h3 class="project-detail">Details</h3>
+                                <h3 class="project-case-study">Case Study</h3>
                                 <p class="indent">Reliable Mobile Labs is a medical lab testing service with locations in several states. The client required a website to showcase their services, generate leads and allow physicians to log into a portal within the website.</p>
                                 <p class="indent last-paragraph">The team at FawkesFX put together the design and concept for this project. I assisted the front end team with client side development using JavaScript, some HTML and CSS. I then created a lead management system using PHP and capped it off with a router to allow for search engine friendly URLs.</p>
                                 <p class="text-center">
@@ -302,7 +302,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 project-details">
-                                <h3 class="project-detail">Details</h3>
+                                <h3 class="project-case-study">Case Study</h3>
                                 <p class="indent">Belssage is a massage therapy provider in Rapid City, SD. This project required a contact form for appointment scheduling, publishing of a quarterly newsletter, easy page updates and a list &amp; description of provided services.</p>
                                 <p class="indent last-paragraph">I designed the website using Photoshop and built the WordPress theme using HTML, CSS, PHP and JavaScript.</p>
                                 <p class="text-center">
@@ -343,7 +343,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 project-details">
-                                <h3 class="project-detail">Details</h3>
+                                <h3 class="project-case-study">Case Study</h3>
                                 <p class="indent">bookMe is a personal project that I built in my free time using PHP5 and MySQL. The goal of this project was to create a fully functional property booking application from scratch using MVC architecture.</p>
                                 <p class="indent last-paragraph">The public site features property listings, a property availability module and a reservation booking module. The administrative interface features a user management module, a reservation management module and a property management module. The property management module comes complete with a rich text editor and image uploader.</p>
                                 <p class="text-center">
@@ -392,7 +392,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 project-details">
-                                <h3 class="project-detail">Details</h3>
+                                <h3 class="project-case-study">Case Study</h3>
                                 <p class="indent">FindMyISP is a personal project that I built in my free time. The primary goals of this project were to inform a user of who their current ISP is and display other ISP options in the user's area.</p>
                                 <p class="indent last-paragraph">The application utilizes the HTML5 Geolocation API to get the user's current location and then uses the IpInfo API to obtain the users ISP. The IpInfo API is also used as a geolocation fallback, should the user's browser not support geolocation. The application then uses the Google Places Web Services API to determine nearby ISPs.</p>
                                 <p class="text-center">
@@ -434,7 +434,7 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 project-details">
-                                <h3 class="project-detail">Details</h3>
+                                <h3 class="project-case-study">Case Study</h3>
                                 <p class="indent">TweetMe is a Twitter feed application that I developed in my free time. The primary goal of this project was to create an application to pull in a controllable number of statuses from a Twitter user's public timeline.</p>
                                 <p class="indent last-paragraph">This application retrieves the statuses via the Twitter REST API, then caches the JSON responses to a flat file 15 minutes to avoid the Twitter API rate limits. Next, TweetMe repackages the cached JSON response to filter out unneeded properties and then outputs the repackaged timeline in either JSON or formatted HTML.</p>
                                 <p class="text-center">

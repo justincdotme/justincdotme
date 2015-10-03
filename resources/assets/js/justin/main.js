45fccca75875eff5bc9f97aed3e19d7f3cbe2da6
Justin Christenson <info@Justinc.me>
2015-10-02 19:21:42 -0700

    Fixed bug where scroll down arrow persisted after scrolling down

--- resources/assets/js/justin/main.js
+++ resources/assets/js/justin/main.js
@@ -18,6 +18,18 @@ $(document).ready(function(){
         });
     });
 
+    //Hide scroll down icon when not on homepage
+    $(window).scroll(function() {
+        $downArrow = $("a#scroll-down");
+        $downArrow.css({
+            display: 'inline'
+        });
+        if ($(this).scrollTop() > $('section#home').height()) {
+            $downArrow.css({
+                'display': 'none'
+            });
+        }
+    });
 
     //Smooth Scrolling
     $('a.scroll').on('click', function(e) {

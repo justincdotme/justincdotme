8fb3a2c04a5ec6208d35e8b1865898e91018dc0e
Justin Christenson <info@Justinc.me>
2015-10-03 18:57:59 -0700

    Fixed Bootstrap's "Mobile nav menu doesn't collapse on link click" bug that they claim is a feature

--- resources/assets/js/justin/main.js
+++ resources/assets/js/justin/main.js
@@ -20,6 +20,12 @@ window.addEventListener("resize", function() {
     justincMe.setHomeHeight();
 }, false);
 
+//Auto close mobile nav when link is clicked
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
 
 $(document).ready(function(){
 

66562b4632fce404dfced7443354f23f64b5060b
Justin Christenson <info@Justinc.me>
2015-10-04 18:19:17 -0700

    Removed testimonial from ProjectsTableSeeder (for now)

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -42,7 +42,7 @@ class ProjectsTableSeeder extends Seeder
             'github_url'            => null,
             'case_study'            => '<p class="indent project-case-p">Belssage is a massage therapy provider in Rapid City, SD. This project required a contact form for appointment scheduling, publishing of a quarterly newsletter, easy page updates and a list &amp; description of provided services.</p>
                                         <p class="indent project-case-p last-paragraph">I designed the website using Photoshop and built the WordPress theme using HTML, CSS, PHP and JavaScript.</p>',
-            'testimonial'           => '<p class="indent testimonial">Justin\'s talent for web design has generated a great deal of business for me as prospective and current clients spend time viewing the websites due to the ease of fiinding answers to questions they may have concerning my services and costs. The attractiveness of the website also gives my business an edge due to the relaxing design which is helpful in the Massage field. His immediate attention to requests for updates and changes keeps my website current and allows my clients to take full advantage of specials and office hours. Justin\'s affordable rates makes it easy to maintain a website as well. I sincerely recommend his services as he not only creates websites but makes them unique to the business and business owner.</p>'
+            'testimonial'           => null
         ]);
 
         DB::table('projects')->insert([
@@ -73,8 +73,8 @@ class ProjectsTableSeeder extends Seeder
             'live_url'              => 'http://findmyisp.demos.justinc.me/',
             'admin_url'             => null,
             'github_url'            => 'https://github.com/justincdotme/findmyisp',
-            'case_study'            => '<p class="indent project-case-p">FindMyISP is a personal project that I built in my free time. The primary goals of this project were to inform a user of who their current ISP is and display other ISP options in the user\'s area.</p>
-                                        <p class="indent project-case-p last-paragraph">The application utilizes the HTML5 Geolocation API to get the user\'s current location and then uses the IpInfo API to obtain the users ISP. The IpInfo API is also used as a geolocation fallback, should the user\'s browser not support geolocation. The application then uses the Google Places Web Services API to determine nearby ISPs.</p>',
+            'case_study'            => '<p class="indent project-case-p">FindMyISP is a personal project that I built in my free time. The primary goals of this project were to inform a user of who their current ISP is and display other ISP options in the user&#39;s area.</p>
+                                        <p class="indent project-case-p last-paragraph">The application utilizes the HTML5 Geolocation API to get the user&#39;s current location and then uses the IpInfo API to obtain the users ISP. The IpInfo API is also used as a geolocation fallback, should the user&#39;s browser not support geolocation. The application then uses the Google Places Web Services API to determine nearby ISPs.</p>',
             'testimonial'           => null
         ]);
 
@@ -84,7 +84,7 @@ class ProjectsTableSeeder extends Seeder
             'live_url'              => 'http://tweetme.demos.justinc.me/',
             'admin_url'             => null,
             'github_url'            => 'https://github.com/justincdotme/tweetme',
-            'case_study'            => '<p class="indent project-case-p">TweetMe is a Twitter feed application that I developed in my free time. The primary goal of this project was to create an application to pull in a controllable number of statuses from a Twitter user\'s public timeline.</p>
+            'case_study'            => '<p class="indent project-case-p">TweetMe is a Twitter feed application that I developed in my free time. The primary goal of this project was to create an application to pull in a controllable number of statuses from a Twitter user&#39;s public timeline.</p>
                                         <p class="indent project-case-p last-paragraph">This application retrieves the statuses via the Twitter REST API, then caches the JSON responses to a flat file 15 minutes to avoid the Twitter API rate limits. Next, TweetMe repackages the cached JSON response to filter out unneeded properties and then outputs the repackaged timeline in either JSON or formatted HTML.</p>',
             'testimonial'           => null
         ]);

b6fef298cbfb66fcd11b9400d7deb9279aff522e
Justin Christenson <info@Justinc.me>
2015-10-04 18:19:41 -0700

    Added eager loading for Project->images() to HomeController Added eager loading for Project->skills() to HomeController Updated home template with loop of projects Updated home SCSS

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Transformers\PageTransformer;
 use App\Models\Page;
+use App\Models\Project;
 use Exception;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -28,11 +29,15 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
+        //Get the projects, eager load the project images and skills
+        $projects = Project::with('images', 'skills')->get();
+
         //Check if the contact submission cookie has been set
         $contacted = ($request->cookie('cconf')) ? $request->cookie('cconf') : false;
 
         return view('public.home')
-            ->withContacted($contacted);
+            ->withContacted($contacted)
+            ->withProjects($projects);
     }
 
     /**

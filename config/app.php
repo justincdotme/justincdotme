32d589289ba2d35f118e2904af8ea13b638b4850
Justin Christenson <info@justinc.me>
2017-03-02 06:17:33 -0800

    JCME-25 Remove unused Dingo API

--- config/app.php
+++ config/app.php
@@ -148,7 +148,6 @@ return [
         /*
          * Third party Service Providers
          */
-        Dingo\Api\Provider\LaravelServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
 
     ],
@@ -199,8 +198,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
     ],
 
 ];

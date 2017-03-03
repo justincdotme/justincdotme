51353a273c660466729dd61551a24e84d9dc3a59
Justin Christenson <info@justinc.me>
2017-03-03 05:55:25 -0800

    JCME-26 Updated analytics URL

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -11,11 +11,11 @@
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
-        var u="//stats.justinc.me/";
+        var u="https://stats.justinc.me/";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
         _paq.push(['setSiteId', 1]);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
     })();
 </script>
-<noscript><p><img src="//stats.justinc.me/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="https://stats.justinc.me/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
